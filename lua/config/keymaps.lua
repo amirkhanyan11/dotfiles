fabf692b440f1686ad1595f22f69e15af8470027
amirkhanyan11 <artyom.amirkhanyan11@gmail.com>
2024-03-11 23:35:18 +0400

    aaaaaaaaaaaaaaa

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,10 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
