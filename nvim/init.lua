d6a64639804c397471cc94731e33915ad53ea336
amirkhanyan11 <artyom.amirkhanyan11@gmail.com>
2025-03-02 21:00:29 +0400

    JANA JANA

---
+++ nvim/init.lua
@@ -0,0 +1,12 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+
+vim.cmd("colorscheme rose-pine")
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.cmd("hi NormalNC guibg=NONE ctermbg=NONE")
+
+-- require("notify").setup({
+--   background_colour= "#000000",
+-- })
---
+++ nvim/lua/plugins/midnight.lua
@@ -0,0 +1,38 @@
+return {
+
+  {'dasupradyumna/midnight.nvim'},
+
+
+  {"rose-pine/neovim"},
+
+  {"navarasu/onedark.nvim"},
+  
+  {"projekt0n/github-nvim-theme"},
+  
+  {"mellow-theme/mellow.nvim"},
+  
+  {"bluz71/vim-nightfly-colors"},
+  
+  {"shaunsingh/solarized.nvim"},
+
+
+{
+
+   "folke/tokyonight.nvim",
+--     opts = {
+--       transparent = true,
+--       styles = {
+--         sidebars = "transparent",
+--         float = "transparent",
+--       },
+--     },
+  },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "midnight",
+    },
+  },
+}
