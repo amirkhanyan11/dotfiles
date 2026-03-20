854702248f97d0727a891ce4cd2abef48b5dbdf5
amirkhanyan11 <artyom.amirkhanyan11@gmail.com>
2026-03-21 03:25:49 +0400

    Trying nvhcad

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -1,9 +1,8 @@
-require "nvchad.mappings"
+-- require "nvchad.mappings"
 
 -- add yours here
 
 local map = vim.keymap.set
-local del = vim.keymap.del
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
@@ -23,7 +22,6 @@ map("n", "N", "Nzzzv")
 
 map("x", "<leader>p", [["_dP]])
 
-
 --- Telescope
 map('n', '<leader><space>', "<cmd>Telescope find_files<cr>", { desc = 'Telescope find files' })
 
@@ -42,40 +40,6 @@ end, { desc = 'Harpoon toggle quick menu'})
 for i = 1, 4 do
     map('n', '<leader>' .. i, function()
         require("harpoon.ui").nav_file(i)
-    end, { desc = 'Harpoon add file'})
+    end, { desc = 'Harpoon goto file' .. i})
 end
-
-
--- Undo nvchads retarded remaps
-
-del("n", ";")
-
--- nvimtree
-del("n", "<C-n>")
-del("n", "<leader>e")
-
--- telescope
-del("n", "<leader>fw")
-del("n", "<leader>fb")
-del("n", "<leader>fh")
-del("n", "<leader>ma")
-del("n", "<leader>fo")
-del("n", "<leader>fz")
-del("n", "<leader>cm")
-del("n", "<leader>gt")
-del("n", "<leader>pt")
-
-del("n", "<leader>th")
-
-del("n", "<leader>ff")
-
-del("n", "<leader>fa")
-
--- terminal
-del("t", "<C-x>")
-
--- new terminals
-del("n", "<leader>h")
-
-del("n", "<leader>v")
 
