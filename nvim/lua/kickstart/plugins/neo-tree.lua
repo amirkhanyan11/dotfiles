f2b82785a6807d1dd3f5cff1b8e8d7dab5bff4ee
amirkhanyan11 <artyom.amirkhanyan11@gmail.com>
2026-03-22 15:28:35 +0400

    Removed nvim

--- nvim/lua/kickstart/plugins/neo-tree.lua
+++
@@ -1,9 +0,0 @@
--- Neo-tree is a Neovim plugin to browse the file system
--- https://github.com/nvim-neo-tree/neo-tree.nvim
-
----@module 'lazy'
----@type LazySpec
-return {
-  'nvim-neo-tree/neo-tree.nvim',
-  enabled = false,
-}
